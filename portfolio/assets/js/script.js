104a0ff50d1dca545e99d6804a2021fd2e239d85
alfaizk64 <alfaizk64@gmail.com>
2024-07-26 20:20:16 +0530

    portfolio

---
+++ portfolio/assets/js/script.js
@@ -0,0 +1,88 @@
+// smooth scrolling by lenis
+const lenis = new Lenis()
+lenis.on('scroll', (e) => {
+  console.log(e)
+})
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+requestAnimationFrame(raf)
+// smooth scrolling by lenis
+// mobile nav js open nnav in mobile and close nav 
+const sideMenu =document.getElementById("sideMenu")
+const menuOpen = document.getElementById("menu-open")
+const menuClose = document.getElementById("menu-close")
+
+menuOpen.addEventListener("click",()=>{
+  sideMenu.style.transform = 'translateX(-16rem)'
+})
+menuClose.addEventListener("click",()=>{
+  sideMenu.style.transform = 'translateX(0rem)'
+  })
+
+
+  // gsap nav menu
+  let tl = gsap.timeline()
+tl.from(".logo",{
+  y:-30,
+  opacity:0,
+  duration:1,
+  ease:"power2.out",
+  delay:0.5
+})
+tl.from(".nav-list li",{
+  y:-30,
+  opacity:0,
+  duration:1,
+  ease:"power2.out",
+  delay:0.5,
+  stagger:0.2
+})
+tl.from(".nav-contact span",{
+  y:-30,
+  opacity:0,
+  duration:1,
+  ease:"power2.out",
+  delay:0.5,
+  stagger:0.2
+})
+tl.from(".nav-contact i",{
+  x:30,
+  opacity:0,
+  duration:1,
+  ease:"power2.out",
+  delay:0.5,
+  stagger:0.2
+})
+
+gsap.from(".fwd",{
+  opacity:0,
+  duration:2,
+  ease:"power2.out",
+  x:500,
+  delay:.5,
+})
+gsap.from(".fsd",{
+  opacity:0,
+  duration:2,
+  ease:"power2.out",
+  x:-500,
+  delay:.5,
+})
+
+
+ //cursore animation
+ let cursor = document.getElementById("cursor")
+ console.log(cursor);
+ let body=document.querySelector("body")
+ 
+body.addEventListener("mousemove",(mouse)=>{
+  gsap.to(cursor,{
+    x:mouse.x,
+    y:mouse.y,
+    duration:0.7,
+    ease:"power2.out",
+   })
+  
+})
---
+++ portfolio/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily:{
+        'Roboto': [ "Roboto"," sans-serif"],
+        'Poppins': ["Poppins", "sans-serif"],
+        'Inter': ["Inter", "sans-serif"],
+        'Open-Sans':["Open Sans", "sans-serif"],
+        'Montserrat':["Montserrat", "sans-serif"],
+        
+      
+      }
+    },
+  },
+  plugins: [],
+}
